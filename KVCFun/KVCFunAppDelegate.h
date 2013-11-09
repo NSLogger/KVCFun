ef505fac5f6f05e04110744f5af5bea1f904ba5e
Arun Agrawal <arunagw@gmail.com>
2013-11-09 21:14:21 +0100

    Initial Commit

---
+++ KVCFun/KVCFunAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KVCFunAppDelegate.h
+//  KVCFun
+//
+//  Created by Arun Agrawal on 09/11/13.
+//  Copyright (c) 2013 Arun Agrawal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface KVCFunAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ KVCFun/KVCFunAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  KVCFunAppDelegate.m
+//  KVCFun
+//
+//  Created by Arun Agrawal on 09/11/13.
+//  Copyright (c) 2013 Arun Agrawal. All rights reserved.
+//
+
+#import "KVCFunAppDelegate.h"
+
+@implementation KVCFunAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
